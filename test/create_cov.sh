b9f4d74b39f356c64b5815788ea951eece81aade
Jannik Glückert <jannik.glueckert@gmail.com>
2021-11-17 02:08:15 +0100

    cov: make llvm utils path changeable by user

--- test/create_cov.sh
+++ test/create_cov.sh
@@ -4,6 +4,9 @@ if [ "$(basename "$(realpath .)")" != "build" ]; then
 	exit 1
 fi
 
+LLVM_PROFDATA=${LLVM_PROFDATA:="llvm-profdata"}
+LLVM_COV=${LLVM_COV:="llvm-cov"}
+
 mapfile -t objs_pre < <(find . -path './test' -prune -o -path './protos' -prune -o -name '*.o' -print)
 objs=()
 for obj in "${objs_pre[@]}"; do
@@ -12,6 +15,6 @@ for obj in "${objs_pre[@]}"; do
 done
 mapfile -t profraws < <(find . -name '*.profraw')
 
-llvm-profdata merge -sparse "${profraws[@]}" -o coverage.profdata
-llvm-cov report --ignore-filename-regex='(third_party\/.*|protos\/.*)' -instr-profile coverage.profdata "${objs[@]}" > coverage.txt
-llvm-cov show --format=html --ignore-filename-regex='(third_party\/.*|protos\/.*)' -instr-profile coverage.profdata "${objs[@]}" > coverage.html
+${LLVM_PROFDATA} merge -sparse "${profraws[@]}" -o coverage.profdata
+${LLVM_COV} report --ignore-filename-regex='(third_party\/.*|protos\/.*)' -instr-profile coverage.profdata "${objs[@]}" > coverage.txt
+${LLVM_COV} show --format=html --ignore-filename-regex='(third_party\/.*|protos\/.*)' -instr-profile coverage.profdata "${objs[@]}" > coverage.html
