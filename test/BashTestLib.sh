1680208f5d68ac0946c3bd48b547a61b75309a24
Jannik Glückert <jannik.glueckert@gmail.com>
2021-11-24 02:43:42 +0100

    test: add ServerMsgHelper

--- test/BashTestLib.sh
+++ test/BashTestLib.sh
@@ -5,7 +5,8 @@ TEST="${1}"
 NAME="${2}"
 CLIENT="env LLVM_PROFILE_FILE=${NAME}-client.profraw ${3}"
 SERVER="env LLVM_PROFILE_FILE=${NAME}-server.profraw ${4}"
-shift 4
+SERVER_HELPER="env LLVM_PROFILE_FILE=/dev/null ${5}"
+shift 5
 
 SOCK="${PWD}/${NAME}.sock"
 export DISTPLUSPLUS_LISTEN_ADDRESS="unix:${SOCK}"
@@ -13,7 +14,9 @@ export DISTPLUSPLUS_LOG_LEVEL="trace"
 
 # shellcheck source=/dev/null
 source "${TEST}"
+set -x
 runTest
 ret=$?
+set +x
 rm -f "${SOCK}"
 exit ${ret}
---
+++ test/ServerMsgHelper.cpp
@@ -0,0 +1,73 @@
+#include "common/common.hpp"
+#include "distplusplus.grpc.pb.h"
+#include "distplusplus.pb.h"
+
+#include <cstdlib>
+#include <google/protobuf/message.h>
+#include <google/protobuf/util/json_util.h>
+#include <grpc/grpc.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+#include <iostream>
+#include <memory>
+#include <string>
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		std::cerr << "ServerMsgHelper: incorrect usage: expected 2 args but got " << argc - 1 << std::endl;
+		return 1;
+	}
+	distplusplus::common::BoundsSpan args(argv, argc);
+	std::string messageType = args[1];
+	std::string messageContent = args[2];
+
+	std::unique_ptr<google::protobuf::Message> message = nullptr;
+	std::unique_ptr<google::protobuf::Message> answer = nullptr;
+	if (messageType == "ServerQuery") {
+		message = std::make_unique<distplusplus::ServerQuery>();
+		answer = std::make_unique<distplusplus::QueryAnswer>();
+	} else if (messageType == "Reservation") {
+		message = std::make_unique<distplusplus::Reservation>();
+		answer = std::make_unique<distplusplus::ReservationAnswer>();
+	} else if (messageType == "CompileRequest") {
+		message = std::make_unique<distplusplus::CompileRequest>();
+		answer = std::make_unique<distplusplus::CompileAnswer>();
+	} else {
+		std::cerr << "ServerMsgHelper: unrecognized message type " << messageType << std::endl;
+		return 1;
+	}
+
+	std::cerr << "ServerMsgHelper: received input " << messageContent << std::endl;
+	google::protobuf::util::JsonStringToMessage(messageContent, message.get());
+	std::string json;
+	google::protobuf::util::MessageToJsonString(*message, &json);
+	std::cerr << "ServerMsgHelper: parsed json as " << json << std::endl;
+
+	const char *server = getenv("DISTPLUSPLUS_LISTEN_ADDRESS");
+	if (server == nullptr) {
+		std::cerr << "ServerMsgHelper: DISTPLUSPLUS_LISTEN_ADDRESS not set!" << std::endl;
+		return 1;
+	}
+	grpc::ChannelArguments channelArgs;
+	channelArgs.SetMaxReceiveMessageSize(INT_MAX);
+	std::unique_ptr<distplusplus::CompilationServer::Stub> stub =
+		distplusplus::CompilationServer::NewStub(grpc::CreateCustomChannel(server, grpc::InsecureChannelCredentials(), channelArgs));
+	grpc::ClientContext context;
+	grpc::Status ret;
+	if (messageType == "ServerQuery") {
+		ret = stub->Query(&context, *dynamic_cast<distplusplus::ServerQuery *>(message.get()),
+						  dynamic_cast<distplusplus::QueryAnswer *>(answer.get()));
+	} else if (messageType == "Reservation") {
+		ret = stub->Reserve(&context, *dynamic_cast<distplusplus::Reservation *>(message.get()),
+							dynamic_cast<distplusplus::ReservationAnswer *>(answer.get()));
+		std::cout << dynamic_cast<distplusplus::ReservationAnswer *>(answer.get())->uuid();
+	} else if (messageType == "CompileRequest") {
+		ret = stub->Distribute(&context, *dynamic_cast<distplusplus::CompileRequest *>(message.get()),
+							   dynamic_cast<distplusplus::CompileAnswer *>(answer.get()));
+	}
+	if (ret.error_code() != grpc::StatusCode::OK) {
+		std::cerr << "ServerMsgHelper: rpc call " << messageType << " returned " << distplusplus::common::mapGRPCStatus(ret.error_code())
+				  << " " << ret.error_message() << std::endl;
+		return 1;
+	}
+}
