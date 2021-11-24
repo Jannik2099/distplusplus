d8f5142359455792ad315c3e38227577a4c33afe
Jannik Glückert <jannik.glueckert@gmail.com>
2021-11-24 12:20:11 +0100

    server: sanitize Tempfile invocation & rudimentary abort

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -90,72 +90,81 @@ grpc::Status Server::Reserve([[maybe_unused]] grpc::ServerContext *context, [[ma
 
 grpc::Status Server::Distribute(grpc::ServerContext *context, const distplusplus::CompileRequest *request,
 								distplusplus::CompileAnswer *answer) {
-	const std::string clientIP = context->peer();
-	const std::string &clientUUID = request->uuid();
-	BOOST_LOG_TRIVIAL(trace) << "client " << clientIP << " sent RPC " << [&request]() {
-		std::string ret;
-		google::protobuf::util::MessageToJsonString(*request, &ret);
-		return ret;
-	}();
-	reservationLock.lock();
-	if (std::erase_if(reservations, [&clientUUID](const _internal::reservationType &a) { return std::get<0>(a) == clientUUID; }) == 0) {
-		reservationLock.unlock();
-		const std::string errorMessage = "error: uuid " + clientUUID + " not in reservation list.";
-		BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " sent job but uuid " << clientUUID << " was not in reservation list";
-		return {grpc::StatusCode::FAILED_PRECONDITION, errorMessage};
-	}
-	reservationLock.unlock();
-	common::ScopeGuard jobCountGuard([&]() { jobsRunning--; });
-
-	// TODO: do this proper
-	if (!sanitizeRequest(*request)) {
-		BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " with job " << clientUUID << " protocol violation!";
-		return {grpc::StatusCode::INVALID_ARGUMENT, "protocol violation"};
-	}
-
-	if (!checkCompilerAllowed(request->compiler())) {
-		const std::string errorMessage = "error: compiler " + request->compiler() + " not in allow list";
-		BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " sent job but compiler " << request->compiler() << " was not in allow list";
-		return {grpc::StatusCode::INVALID_ARGUMENT, errorMessage};
-	}
-
-	const distplusplus::common::Tempfile inputFile(clientIP + "." + request->inputfile().name(), request->inputfile().content());
-
-	const distplusplus::common::Tempfile outputFile(clientIP + "." + request->inputfile().name() + ".o");
-
-	std::vector<std::string_view> preArgs(request->argument().begin(), request->argument().end());
-	std::vector<std::string_view> args;
 	try {
-		parser::Parser parser(preArgs);
-		args = parser.args();
-	} catch (const parser::CannotProcessSignal &signal) {
-		BOOST_LOG_TRIVIAL(warning) << "job " << clientUUID << " from host " << clientIP << " aborted due to invalid arguments:\n"
-								   << signal.what();
-		return {grpc::StatusCode::INVALID_ARGUMENT, "job aborted due to invalid arguments:\n" + signal.what()};
+		const std::string clientIP = context->peer();
+		const std::string &clientUUID = request->uuid();
+		BOOST_LOG_TRIVIAL(trace) << "client " << clientIP << " sent RPC " << [&request]() {
+			std::string ret;
+			google::protobuf::util::MessageToJsonString(*request, &ret);
+			return ret;
+		}();
+		reservationLock.lock();
+		if (std::erase_if(reservations, [&clientUUID](const _internal::reservationType &a) { return std::get<0>(a) == clientUUID; }) == 0) {
+			reservationLock.unlock();
+			const std::string errorMessage = "error: uuid " + clientUUID + " not in reservation list.";
+			BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " sent job but uuid " << clientUUID << " was not in reservation list";
+			return {grpc::StatusCode::FAILED_PRECONDITION, errorMessage};
+		}
+		reservationLock.unlock();
+		common::ScopeGuard jobCountGuard([&]() { jobsRunning--; });
+
+		// TODO: do this proper
+		if (!sanitizeRequest(*request)) {
+			BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " with job " << clientUUID << " protocol violation!";
+			return {grpc::StatusCode::INVALID_ARGUMENT, "protocol violation"};
+		}
+
+		if (!checkCompilerAllowed(request->compiler())) {
+			const std::string errorMessage = "error: compiler " + request->compiler() + " not in allow list";
+			BOOST_LOG_TRIVIAL(warning) << "client " << clientIP << " sent job but compiler " << request->compiler()
+									   << " was not in allow list";
+			return {grpc::StatusCode::INVALID_ARGUMENT, errorMessage};
+		}
+
+		// the client could be a path to an unix socket too
+		std::string clientIPDelimited = clientIP;
+		std::replace(clientIPDelimited.begin(), clientIPDelimited.end(), '/', '-');
+		const distplusplus::common::Tempfile inputFile(clientIPDelimited + "." + request->inputfile().name(), request->inputfile().content());
+
+		const distplusplus::common::Tempfile outputFile(clientIPDelimited + "." + request->inputfile().name() + ".o");
+
+		std::vector<std::string_view> preArgs(request->argument().begin(), request->argument().end());
+		std::vector<std::string_view> args;
+		try {
+			parser::Parser parser(preArgs);
+			args = parser.args();
+		} catch (const parser::CannotProcessSignal &signal) {
+			BOOST_LOG_TRIVIAL(warning) << "job " << clientUUID << " from host " << clientIP << " aborted due to invalid arguments:\n"
+									   << signal.what();
+			return {grpc::StatusCode::INVALID_ARGUMENT, "job aborted due to invalid arguments:\n" + signal.what()};
+		}
+
+		args.emplace_back("-o");
+		args.emplace_back(outputFile.c_str());
+		args.emplace_back(inputFile.c_str());
+
+		const boost::filesystem::path compilerPath = boost::process::search_path(request->compiler());
+		std::vector<std::string> processArgs;
+		processArgs.reserve(args.size());
+		for (const auto &arg : args) {
+			processArgs.emplace_back(arg);
+		}
+		distplusplus::common::ProcessHelper compilerProcess(compilerPath, processArgs);
+		// TODO: proper logging
+
+		answer->set_returncode(compilerProcess.returnCode());
+		answer->set_stdout(compilerProcess.get_stdout());
+		answer->set_stderr(compilerProcess.get_stderr());
+		answer->mutable_outputfile()->set_name(outputFile);
+		std::ifstream fileStream(outputFile);
+		std::stringstream fileContent;
+		fileContent << fileStream.rdbuf();
+		answer->mutable_outputfile()->set_content(fileContent.str());
+		return grpc::Status::OK;
+	} catch (const std::exception &e) {
+		std::cerr << "exception in gRPC function: " << e.what() << std::endl;
+		std::abort();
 	}
-
-	args.emplace_back("-o");
-	args.emplace_back(outputFile.c_str());
-	args.emplace_back(inputFile.c_str());
-
-	const boost::filesystem::path compilerPath = boost::process::search_path(request->compiler());
-	std::vector<std::string> processArgs;
-	processArgs.reserve(args.size());
-	for (const auto &arg : args) {
-		processArgs.emplace_back(arg);
-	}
-	distplusplus::common::ProcessHelper compilerProcess(compilerPath, processArgs);
-	// TODO: proper logging
-
-	answer->set_returncode(compilerProcess.returnCode());
-	answer->set_stdout(compilerProcess.get_stdout());
-	answer->set_stderr(compilerProcess.get_stderr());
-	answer->mutable_outputfile()->set_name(outputFile);
-	std::ifstream fileStream(outputFile);
-	std::stringstream fileContent;
-	fileContent << fileStream.rdbuf();
-	answer->mutable_outputfile()->set_content(fileContent.str());
-	return grpc::Status::OK;
 }
 
 Server::Server() {
