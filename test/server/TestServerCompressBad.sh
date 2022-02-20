1041f39d7517d58018b1ec5d01cd20fc3a90a9ae
Jannik Glückert <jannik.glueckert@gmail.com>
2022-02-21 00:53:36 +0100

    add missing compression tests

---
+++ test/server/TestServerCompressBad.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function runTest() {
+    (sleep 1; ${SERVER} --compression THISCOMPRESSIONDOESNOTEXIST) &
+    PID=$!
+    sleep 3
+    kill ${PID}
+    wait ${PID}
+    RET=$?
+    if [[ "${RET}" == "1" ]]; then
+        return 0
+    fi
+    return 1
+}
---
+++ test/server/TestServerCompressNONE.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function runTest() {
+    ${SERVER} --compression NONE &
+    PID=$!
+    sleep 1
+
+    UUID=$(${SERVER_HELPER} "Reservation" "{}")
+    JSON_FMT='{"uuid":"%s","compiler":"%s","inputFile":%s,"cwd":"%s","argument":%s}'
+    JSON=$(printf "${JSON_FMT}" "${UUID}" 'cc' "${DUMMY_FILE}" "$(pwd)" '["-c"]')
+    ${SERVER_HELPER} "CompileRequest" "${JSON}"
+    RET=$?
+    if [[ "${RET}" != "0" ]]; then
+        echo "SERVER_HELPER failed unexpectedly"
+        return ${RET}
+    fi
+    
+    (sleep 1; kill ${PID}) &
+    wait ${PID}
+    RET=$?
+    if [[ "${RET}" == "128" ]]; then
+        echo "SERVER died before being terminated"
+        return ${RET}
+    fi
+    
+    return 0
+}
